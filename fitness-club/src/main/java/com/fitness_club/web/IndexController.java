a0b082dc8c45389bb2c1bbb615b03a9fe0837ef3
NikolaiK420 <nikolaikumanov006@gmail.com>
2025-03-18 22:04:03 +0200

    login and register

--- fitness-club/src/main/java/com/fitness_club/web/IndexController.java
+++ fitness-club/src/main/java/com/fitness_club/web/IndexController.java
@@ -1,7 +1,10 @@
 package com.fitness_club.web;
 
+import com.fitness_club.web.dto.LoginRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
@@ -10,6 +13,20 @@ public class IndexController {
     public String getIndexPage() {
 
         return "index";
+    }
+
+    @GetMapping("/login")
+    public ModelAndView getLoginPage(@RequestParam(value = "error", required = false) String errorParam) {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        modelAndView.addObject("loginRequest", new LoginRequest());
+
+        if (errorParam != null) {
+            modelAndView.addObject("errorMessage", "Incorrect username or password!");
+        }
+
+        return modelAndView;
     }
 
 }
---
+++ fitness-club/src/main/java/com/fitness_club/web/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.fitness_club.web.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Size;
+
+@Data
+public class LoginRequest {
+
+    @Size(min = 6, message = "Username must be at least 6 symbols")
+    private String username;
+
+    @Size(min = 6, message = "Password must be at least 6 symbols")
+    private String password;
+}
