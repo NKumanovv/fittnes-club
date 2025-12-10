5b7b7858fe9901ead62b7c4f26eac00949c9e99d
NKumanovv <nikolaikumanov006@gmail.com>
2025-12-10 16:53:46 +0200

    Remove FitnessPlan and Medal domain modules
    Deleted model, repository, and service classes for FitnessPlan and Medal features. This refactor removes related entities and supporting code, likely due to a change in requirements or feature deprecation.

--- fitness-club/src/main/java/com/fitness_club/fitnessPlan/service/FitnessPlanService.java
+++
@@ -1,9 +0,0 @@
-package com.fitness_club.fitnessPlan.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class FitnessPlanService {
-
-
-}
--- fitness-club/src/main/java/com/fitness_club/medal/service/MedalService.java
+++
@@ -1,7 +0,0 @@
-package com.fitness_club.medal.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class MedalService {
-}
